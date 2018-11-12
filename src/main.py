93446b25534f6a46bf28b3139c0732f6388574ad
mitep <milos.tepic@yahoo.com>
2018-11-12 10:27:13 +0100

    Add start_worker func in main module

--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
+import sys
+import os
 from workers.cur_state import CurrentState
+from workers.change_encoding import ChangeEncoding
 from utils import logger_util
+from utils import arg_data
 
-logger = logger_util.create_logger('main_logger')
-logger.info('main logger')
+def start_worker(worker_type, config):
+    logger = logger_util.create_logger('main_logger')
+    logger.info('Starting TAU ...')
 
-data = {}
-data['dir_path'] = 'path to music dir'
-data['state_path'] = 'path where state yaml file will be created'
+    worker = None
+    if worker_type == 'current_state':
+        worker = CurrentState('calc_cur_state')
+        logger.info('Starting calculate current state worker')
+    elif worker_type == 'change_encoding':
+        worker = ChangeEncoding('encoder')
+        logger.info('Starting encoding worker')
+    else:
+        logger.warning('No worker type ' + worker_type)
+        return
 
-current = CurrentState('current_state_logger')
-current.run(data)
+    data = arg_data.get_data(config)
+    worker.run(data)
+
+if __name__ == '__main__':
+    start_worker('current_state', 'data_path')
