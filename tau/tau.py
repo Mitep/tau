f3d1e34e27793395de777217137881b794ee2312
mitep <milos.tepic@yahoo.com>
2019-01-27 18:21:24 +0100

    Implement main function

--- tau/tau.py
+++ tau/tau.py
@@ -3,16 +3,41 @@ Main module
 Used for arg parsing and calling tau services
 """
 
+import sys
+
 import common
 # import core
 # import extra
 
 
+def runner(params):
+    """
+    Run services based on given parameters
+    """
+
+    logger = common.Logger.get()
+
+    logger.info("Running service")
+    logger.info(f"Input parameters: {params}")
+
+
 def main():
     """
+    Main function
+    Getting user args and starting tau services
     """
+
     logger = common.Logger.get()
+
     logger.info("Starting tau")
+
+    args = sys.argv[1:]
+
+    try:
+        while True:
+            runner(args)
+    except KeyboardInterrupt:
+        logger.info("Exiting tau...")
 
 
 if __name__ == "__main__":
